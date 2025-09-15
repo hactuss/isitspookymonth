b77609658a9363d3effce0950838b997d599de71
hactuss <hactuss@proton.me>
2025-09-15 17:24:59 +0200

    new file:   README.md 	new file:   deno.lock 	new file:   package.json 	new file:   src/.DS_Store 	new file:   src/app.css 	new file:   src/app.d.ts 	new file:   src/app.html 	new file:   src/demo.spec.ts 	new file:   src/lib/.DS_Store 	new file:   src/lib/assets/.DS_Store 	new file:   src/lib/assets/360_F_918673940_RFu8V1NIbJbL7SzKJ3KlmsNKEIREH3XZ.png 	new file:   src/lib/assets/Archivo_Black.zip 	new file:   src/lib/assets/Ultra.zip 	new file:   src/lib/assets/Ultra/LICENSE.txt 	new file:   src/lib/assets/Ultra/Ultra-Regular.ttf 	new file:   src/lib/assets/favicon.svg 	new file:   src/lib/assets/spooky.png 	new file:   src/lib/index.ts 	new file:   src/routes/+layout.svelte 	new file:   src/routes/+page.svelte 	new file:   src/routes/page.svelte.spec.ts 	new file:   static/robots.txt 	new file:   svelte.config.js 	new file:   tsconfig.json 	new file:   vite.config.ts 	new file:   vitest-setup-client.ts

---
+++ src/routes/+page.svelte
@@ -0,0 +1,29 @@
+<script lang="ts">
+    let yesorno:string = "yes"
+    let date = new Date();
+    console.log("test");
+    console.log(date.getMonth());
+    if(new Date.getMonth() = 9){
+
+    }else{
+    }
+</script>
+
+<main class="">
+    <h3>Welcome at isitspookymonthdotcom</h3>
+    <h1>Is it spookymonth?</h1>
+    <h2>{yesorno}</h2>
+    <div style="display: none;" id="spookykids">
+        <img
+            src="https://media.tenor.com/agWRnwIHk0MAAAAj/%D0%B6%D1%83%D1%82%D0%BA%D0%B8%D0%B9%D0%BC%D0%B5%D1%81%D1%8F%D1%86-%D1%81%D0%BA%D0%B8%D0%B4%D0%B8%D0%BF%D0%B0%D0%BC%D0%BF.gif"
+        />
+        <img
+            src="https://media1.tenor.com/m/HrVavLTmWewAAAAd/spooky-month-spooky-dance.gif"
+            alt=""
+        />
+        <img
+            src="https://media1.tenor.com/m/MUKNVPLzWsQAAAAd/itisaspookymonth.gif"
+            alt=""
+        />
+    </div>
+</main>
