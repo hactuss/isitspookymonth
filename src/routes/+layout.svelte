cb1bd4aefd6c7f16b74f34adcf178a304f28022f
hactuss <157395485+hactuss@users.noreply.github.com>
2025-09-16 15:36:17 +0200

    +layout.svelte aktualisieren

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -8,6 +8,7 @@
 <svelte:head>
     <link rel="icon" href={favicon} />
     <title>Is it spooky month?</title>
+    <link rel="apple-touch-icon" href={favicon} type="image/png" />
 </svelte:head>
 
 {@render children?.()}
