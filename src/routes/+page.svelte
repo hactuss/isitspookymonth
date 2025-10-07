21cec75c638a17ff166551945153f098ca907c8e
hactuss <hactuss@proton.me>
2025-10-07 19:19:59 +0200

    modified:   src/app.css 	modified:   src/routes/+page.svelte

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,10 +1,9 @@
 <script lang="ts">
     let fkingerror = " >> Spookymonth by SrPelo << ";
-    let yesorno = "no";
     let d = new Date();
 </script>
 
-<main class="">
+<main>
     <h3>
         Welcome at isitspookymonthdotcom | made by <a
             href="https://hactuss-website.vercel.app">hactuss</a
@@ -17,10 +16,13 @@
         </a> here
     </h3>
     <h1>Is it spookymonth?</h1>
-    <h2>{yesorno}</h2>
-
     {#if d.getMonth() == 9}
-        <div id="spookykids">
+        <p>yes</p>
+    {:else}
+        <p>no</p>
+    {/if}
+    {#if d.getMonth() == 9}
+        <div>
             <h1>Happy Spooky Month</h1>
             <img
                 src="https://media.tenor.com/agWRnwIHk0MAAAAj/%D0%B6%D1%83%D1%82%D0%BA%D0%B8%D0%B9%D0%BC%D0%B5%D1%81%D1%8F%D1%86-%D1%81%D0%BA%D0%B8%D0%B4%D0%B8%D0%BF%D0%B0%D0%BC%D0%BF.gif"
