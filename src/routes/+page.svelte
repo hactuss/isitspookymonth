49ad5a8fc152d094094217fd8c6ae6f677ce9969
hactuss <hactuss@proton.me>
2025-10-07 17:30:52 +0200

    modified:   src/routes/+page.svelte

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,18 +1,7 @@
 <script lang="ts">
     let fkingerror = " >> Spookymonth by SrPelo << ";
     let yesorno = "no";
-    let display = "none";
-    let margin = "25vh";
-    let date = new Date();
-    console.log("test");
-    const month = date.getMonth();
-    console.log(month);
-    if (month == 9) {
-        yesorno = "Yes";
-        display = "block";
-        let margin = "0px";
-    } else {
-    }
+    let d = new Date();
 </script>
 
 <main class="">
@@ -29,20 +18,32 @@
     </h3>
     <h1>Is it spookymonth?</h1>
     <h2>{yesorno}</h2>
-    <div style="display: {display};" id="spookykids">
-        <h1 style="display: {display};">Happy Spooky Month</h1>
-        <img
-            src="https://media.tenor.com/agWRnwIHk0MAAAAj/%D0%B6%D1%83%D1%82%D0%BA%D0%B8%D0%B9%D0%BC%D0%B5%D1%81%D1%8F%D1%86-%D1%81%D0%BA%D0%B8%D0%B4%D0%B8%D0%BF%D0%B0%D0%BC%D0%BF.gif"
-            alt=""
-        />
-        <img
-            src="https://media1.tenor.com/m/HrVavLTmWewAAAAd/spooky-month-spooky-dance.gif"
-            alt=""
-        />
-        <img
-            src="https://media1.tenor.com/m/MUKNVPLzWsQAAAAd/itisaspookymonth.gif"
-            alt=""
-        />
-        <iframe width="560" height="315" src="https://www.youtube.com/embed/xuJM6Y-ljbo?si=0jrf75z8BS8lalw_" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>
-    </div>
+
+    {#if d.getMonth() == 9}
+        <div id="spookykids">
+            <h1>Happy Spooky Month</h1>
+            <img
+                src="https://media.tenor.com/agWRnwIHk0MAAAAj/%D0%B6%D1%83%D1%82%D0%BA%D0%B8%D0%B9%D0%BC%D0%B5%D1%81%D1%8F%D1%86-%D1%81%D0%BA%D0%B8%D0%B4%D0%B8%D0%BF%D0%B0%D0%BC%D0%BF.gif"
+                alt=""
+            />
+            <img
+                src="https://media1.tenor.com/m/HrVavLTmWewAAAAd/spooky-month-spooky-dance.gif"
+                alt=""
+            />
+            <img
+                src="https://media1.tenor.com/m/MUKNVPLzWsQAAAAd/itisaspookymonth.gif"
+                alt=""
+            />
+            <iframe
+                width="560"
+                height="315"
+                src="https://www.youtube.com/embed/xuJM6Y-ljbo?si=0jrf75z8BS8lalw_"
+                title="YouTube video player"
+                frameborder="0"
+                allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+                referrerpolicy="strict-origin-when-cross-origin"
+                allowfullscreen
+            ></iframe>
+        </div>
+    {/if}
 </main>
